0b95afa778b99d9e9de772006555309b74ed32f4
Sine Striker <trueful@163.com>
2024-04-23 20:58:14 +0800

    Update

--- cmake/QMSetupAPI.cmake
+++ cmake/QMSetupAPI.cmake
@@ -853,6 +853,49 @@ function(qm_get_subdirs _var)
     set(${_var} ${_res} PARENT_SCOPE)
 endfunction()
 
+#[[
+    Create the names of output files preserving relative dirs. (Ported from MOC command)
+
+    qm_make_output_file(<infile> <prefix> <ext> <OUT>)
+#]]
+function(qm_make_output_file _infile _prefix _ext _out)
+    string(LENGTH ${CMAKE_CURRENT_BINARY_DIR} _binlength)
+    string(LENGTH ${_infile} _infileLength)
+    set(_checkinfile ${CMAKE_CURRENT_SOURCE_DIR})
+
+    if(_infileLength GREATER _binlength)
+        string(SUBSTRING "${_infile}" 0 ${_binlength} _checkinfile)
+
+        if(_checkinfile STREQUAL "${CMAKE_CURRENT_BINARY_DIR}")
+            file(RELATIVE_PATH _rel ${CMAKE_CURRENT_BINARY_DIR} ${_infile})
+        else()
+            file(RELATIVE_PATH _rel ${CMAKE_CURRENT_SOURCE_DIR} ${_infile})
+        endif()
+    else()
+        file(RELATIVE_PATH _rel ${CMAKE_CURRENT_SOURCE_DIR} ${_infile})
+    endif()
+
+    if(CMAKE_HOST_WIN32 AND _rel MATCHES "^([a-zA-Z]):(.*)$") # absolute path
+        set(_rel "${CMAKE_MATCH_1}_${CMAKE_MATCH_2}")
+    endif()
+
+    set(_outfile "${CMAKE_CURRENT_BINARY_DIR}/${_rel}")
+    string(REPLACE ".." "__" _outfile ${_outfile})
+    get_filename_component(_outpath ${_outfile} PATH)
+
+    if(CMAKE_VERSION VERSION_LESS "3.14")
+        get_filename_component(_outfile_ext ${_outfile} EXT)
+        get_filename_component(_outfile_ext ${_outfile_ext} NAME_WE)
+        get_filename_component(_outfile ${_outfile} NAME_WE)
+        string(APPEND _outfile ${_outfile_ext})
+    else()
+        get_filename_component(_outfile ${_outfile} NAME_WLE)
+    endif()
+
+    file(MAKE_DIRECTORY ${_outpath})
+    set(${_out} ${_outpath}/${_prefix}${_outfile}.${_ext} PARENT_SCOPE)
+endfunction()
+
 # ----------------------------------
 # Private functions
 # ----------------------------------
